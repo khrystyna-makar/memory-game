03fd175fb74399028a74775be9a4e6d72df03287
Khrystyna <khrystyna.makar@gmail.com>
2025-02-01 13:36:42 +0200

    added project

---
+++ src/components/Form.jsx
@@ -0,0 +1,24 @@
+import RegularButton from './RegularButton'
+import Select from './Select'
+import { useRef, useEffect } from 'react'
+
+export default function Form({ handleSubmit, handleChange, isFirstRender }) {
+    const divRef = useRef(null);
+
+    useEffect(() => {
+       !isFirstRender && divRef.current.focus();
+    }, [])
+    return (
+        <div className="form-container" ref={divRef} tabIndex={-1}>
+            <p className="p--regular">
+                Customize the game by selecting an emoji category and a number of memory cards.
+            </p>
+            <form className="wrapper">
+                <Select handleChange={handleChange} />
+                <RegularButton handleClick={handleSubmit}>
+                    Start Game
+                </RegularButton>
+            </form>
+        </div>
+    )
+}
